ba9ab74e9e9745a8351b970543d0c475070ff3b7
Julius <julius.vlasovas@gmail.com>
2020-11-13 21:16:49 +0200

    Im so bad

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,9 +12,17 @@ class MainController extends AbstractController
      * @Route("/")
      * @Method({"GET"})
      */
-    public function index()
+    public function getter()
     {
-        // return new Response('<div class={name}>/</div>');
-        return $this->render('holidays/index.html.twig');
+        $file = file_get_contents("https://kayaposoft.com/enrico/json/v2.0?action=getHolidaysForMonth&month=1&year=2022&country=ltu&region=bw&holidayType=public_holiday");
+        
+        
+
+     
+
+            return $this->render('holidays/index.html.twig', array('name' => $file));
+        
+
+        
     }
 }
