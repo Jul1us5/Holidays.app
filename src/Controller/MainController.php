276f91e5f3327737392a12802839c46e85498a2c
Julius <julius.vlasovas@gmail.com>
2020-11-23 15:17:59 +0200

    up

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -19,9 +19,29 @@ class MainController extends AbstractController
      */
     public function index(Request $request)
     {
+        function getAllSuppCountrys()
+        {
+            $str = file_get_contents('https://kayaposoft.com/enrico/json/v2.0?action=getSupportedCountries');
+            $json = json_decode($str, true);
 
-        // dd($request);
+            $countrys = [];
+            for ($i = 0; $i < count($json); $i++) {
+                array_push($countrys, [$json[$i]['countryCode'],$json[$i]['fullName']]);
+            }
+            return $countrys;
+        }
 
-        return $this->render('holidays/index.html.twig');
+        // $getCountry = $request->query->get('country');
+        // $getRegion = $request->query->get('region');
+        // $getYear = $request->query->get('year');
+
+        // $str = file_get_contents('https://kayaposoft.com/enrico/json/v2.0?action=getHolidaysForYear&year=2022&region=act&country=aus&holidayType=public_holiday');
+        // $json = json_decode($str, true);
+
+
+        
+        // dd($getCountry);
+
+        return $this->render('holidays/index.html.twig', array('country' => getAllSuppCountrys()));
     }
 }
